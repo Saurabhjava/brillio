e34e5fc3f1b35ee096b0bf76163d38b2e7e52958
Saurabhjava <45967591+Saurabhjava@users.noreply.github.com>
2024-04-18 10:37:55 +0530

    Add files via upload

--- calculation-service/src/main/java/com/brillio/proxy/ExchangeProxy.java
+++ calculation-service/src/main/java/com/brillio/proxy/ExchangeProxy.java
@@ -1,12 +1,15 @@
 package com.brillio.proxy;
 
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.brillio.model.CalculationBean;
 
-@FeignClient(name="exchange-service",url = "http://localhost:8080")
+//@FeignClient(name="exchange-service",url = "http://localhost:8080")
+@FeignClient(name="exchange-service")
+@LoadBalancerClient
 public interface ExchangeProxy {
 	@GetMapping("exchange/from/{from}/to/{to}")
 	public CalculationBean getExchangeValue(@PathVariable String from, @PathVariable String to);
